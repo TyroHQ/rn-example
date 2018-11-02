a7c4520d74e909eafa17643ac1ebad12497f9e5c
Christopher Little <christopher@littlehq.uk>
2018-11-02 17:34:17 +0300

    simplify config

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Expo from "expo";
 import { App } from "./components/App";
 import StoryBook from "./storybook";
 
+// TODO: conditionally apply this more intelligently - __DEV__ doesn't seem to work.
 const main = true ? StoryBook : App;
 
 Expo.registerRootComponent(main);
