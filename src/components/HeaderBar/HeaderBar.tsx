97a0a509329944773be1cec562046b446ddd09d1
Engiribait Bruno <bruno@getpoints.com>
2018-11-06 11:32:22 +0300

    Header bar

---
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const HeaderBarContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class HeaderBar extends React.Component<{}> {
+  static defaultProps: HeaderBar["props"];
+  render() {
+    return (
+      <Placeholder>
+        <Placeholder orientation="row">
+          <Placeholder>Menu icon </Placeholder>
+          <Placeholder> Logo </Placeholder>
+        </Placeholder>
+      </Placeholder>
+    );
+  }
+}
