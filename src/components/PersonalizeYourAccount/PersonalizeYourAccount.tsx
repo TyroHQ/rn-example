ad56fa94f5339bfc2ce50420060049b24969237f
Kirunda Brian <brian@getpoints.com>
2018-11-06 11:25:46 +0300

    Added personilize your account

---
+++ src/components/PersonalizeYourAccount/PersonalizeYourAccount.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const PersonalizeYourAccountContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class PersonalizeYourAccount extends React.Component<{}> {
+  static defaultProps: PersonalizeYourAccount["props"];
+  render() {
+    return (
+      <Placeholder>
+        <Placeholder>Header bar</Placeholder>
+        <Placeholder>
+          Content area
+          <Placeholder>Header text</Placeholder>
+          <Placeholder>Explainer text</Placeholder>
+          <Placeholder>User icon</Placeholder>
+          <Placeholder>Explainer text</Placeholder>
+          <Placeholder>
+            Input fields
+            <Placeholder>Input textfield</Placeholder>
+            <Placeholder>Input textfield</Placeholder>
+            <Placeholder>Input textfield</Placeholder>
+          </Placeholder>
+          <Placeholder>Explainer text</Placeholder>
+          <Placeholder>
+            Radio buttons
+            <Placeholder>
+              Radio button1
+              <Placeholder>Selected radiobox</Placeholder>
+              <Placeholder>Input placeholder</Placeholder>
+            </Placeholder>
+            <Placeholder>
+              Radio button2
+              <Placeholder>Unselected radiobox</Placeholder>
+              <Placeholder>Input placeholder</Placeholder>
+            </Placeholder>
+          </Placeholder>
+          <Placeholder>
+            Rectangular buttons
+            <Placeholder>Rectangular buttonleft</Placeholder>
+            <Placeholder>Rectangular buttonright</Placeholder>
+          </Placeholder>
+        </Placeholder>
+      </Placeholder>
+    );
+  }
+}
