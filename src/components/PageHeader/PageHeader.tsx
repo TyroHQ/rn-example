1ddecebf3bde7daae7b26cc91b956c2ec76a74df
Kirunda Brian <brian@getpoints.com>
2018-11-06 11:44:40 +0300

    Added page header

---
+++ src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const PageHeaderContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class PageHeader extends React.Component<{}> {
+  static defaultProps: PageHeader["props"];
+  render() {
+    return (
+      <Placeholder>
+        <Placeholder>
+          PageHeader Component
+          <Placeholder>
+            PageHeader content
+            <Placeholder>Back icon</Placeholder>
+            <Placeholder>Header text</Placeholder>
+          </Placeholder>
+        </Placeholder>
+      </Placeholder>
+    );
+  }
+}
