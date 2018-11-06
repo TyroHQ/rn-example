60a1db986f8a09caeca95f56a966607639bd28a8
Kirunda Brian <brian@getpoints.com>
2018-11-06 12:11:35 +0300

    Added small button

---
+++ src/components/SmallButton/SmallButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const SmallButtonContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class SmallButton extends React.Component<{}> {
+  static defaultProps: SmallButton["props"];
+  render() {
+    return (
+      <Placeholder>
+        <Placeholder>SmallSquareButton</Placeholder>
+      </Placeholder>
+    );
+  }
+}
