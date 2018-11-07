4378baeb5674b42576dc5cb751987fcef37514a0
Kirunda Brian <brian@getpoints.com>
2018-11-07 15:35:25 +0300

    Added Ratings Bar

---
+++ src/components/RatingsBar/RatingsBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const RatingsBarContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class RatingsBar extends React.Component<{}> {
+  static defaultProps: RatingsBar["props"];
+  render() {
+    return (
+      <Placeholder>
+        Ratings bar
+        <Placeholder>Rectangle 2.5</Placeholder>
+        <Placeholder>Rectangle 2.4</Placeholder>
+        <Placeholder>Explainer text - 5 stars</Placeholder>
+        <Placeholder>Explainer text - 5 people</Placeholder>
+      </Placeholder>
+    );
+  }
+}
---
+++ src/components/RatingsBar/RatingsBar.usage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { boolean, text, number } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { RatingsBar } from "./RatingsBar";
+
+export const basic = () => <RatingsBar />;
