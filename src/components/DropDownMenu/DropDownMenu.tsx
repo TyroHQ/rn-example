ab5096905b3e92a71099b156178e26886fafabdc
Kirunda Brian <brian@getpoints.com>
2018-11-07 15:49:12 +0300

    Added drop down menu

---
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const DropDownMenuContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class DropDownMenu extends React.Component<{}> {
+  static defaultProps: DropDownMenu["props"];
+  render() {
+    return <Placeholder>Drop down menu</Placeholder>;
+  }
+}
