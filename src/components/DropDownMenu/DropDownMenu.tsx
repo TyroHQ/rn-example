971a2457839e9fd5e24e9741ab508662e0c802d0
Kirunda Brian <brian@getpoints.com>
2018-11-08 10:12:04 +0300

    Added missing items in the drop down menu heirachy

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -11,6 +11,18 @@ const DropDownMenuContainer = styled.View`
 export class DropDownMenu extends React.Component<{}> {
   static defaultProps: DropDownMenu["props"];
   render() {
-    return <Placeholder>Drop down menu</Placeholder>;
+    return (
+      <Placeholder>
+        Drop down menu
+        <Placeholder>selected text option</Placeholder>
+        <Placeholder>Angle down icon</Placeholder>
+        <Placeholder>
+          Unselected list options
+          <Placeholder>Option 1</Placeholder>
+          <Placeholder>Option 2</Placeholder>
+          <Placeholder>Option 3</Placeholder>
+        </Placeholder>
+      </Placeholder>
+    );
   }
 }
