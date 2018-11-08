fee6dd26f312b83c6e34acd08be5ebf5d91d4d55
Kirunda Brian <brian@getpoints.com>
2018-11-08 10:43:46 +0300

    Added Reward Tile Header

---
+++ src/components/RewardTileHeader/RewardTileHeader.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Placeholder } from "../Placeholder";
+
+const RewardTileHeaderContainer = styled.View`
+  background-color: lightgrey;
+  padding: 10px;
+`;
+
+export class RewardTileHeader extends React.Component<{}> {
+  static defaultProps: RewardTileHeader["props"];
+  render() {
+    return (
+      <Placeholder>
+        Green background Rectangle
+        <Placeholder>
+          Reward tile frame
+          <Placeholder>
+            Left Aligned Text
+            <Placeholder>Offer Headline</Placeholder>
+            <Placeholder>Requirement</Placeholder>
+          </Placeholder>
+          <Placeholder>Unlocked</Placeholder>
+        </Placeholder>
+        <Placeholder>Drop down angle icon</Placeholder>
+      </Placeholder>
+    );
+  }
+}
