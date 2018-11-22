340773a53a2d94173b4d0d1b50317216a8254470
Edgar <edgar@getpoints.com>
2018-11-22 15:20:13 +0300

    added Table Row Component

--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 import styled from 'styled-components'
+import { Placeholder } from "../Placeholder";
 
 const TableRowContainer = styled(View)`
   background-color: lightgrey;
@@ -11,7 +12,12 @@ export class TableRow extends React.Component<{}> {
   static defaultProps: TableRow["props"];
   render() {
     return <TableRowContainer>
-        <Text>TableRow</Text>
-      </TableRowContainer>
+      <Placeholder>Table Row Container
+          <Placeholder>Table Row Text</Placeholder>
+        <Placeholder>Table Row Text</Placeholder>
+        <Placeholder>Table Row Text</Placeholder>
+        <Placeholder>Table Row Text</Placeholder>
+      </Placeholder>
+    </TableRowContainer>
   }
 }
